9faae09011a6c364b5cae80ef0ea8e316699bd61
Bernard Olaires <bolairesjr@gmail.com>
2022-06-30 10:05:11 -0700

    fizzbuzz

--- assignments/week4/fizzBuzz/script.js
+++ assignments/week4/fizzBuzz/script.js
@@ -5,10 +5,12 @@ for (var i=1; i < 101; i++) {
         console.log("FizzBuzz");
     }
     else if (i % 3 == 0) {
-        console.log("fizz");
+        console.log("Fizz");
     }
     else if (i % 5 == 0) {
         console.log("Buzz");
     }
-    else console.log(i)
+    else {
+        console.log(i);
+    }
 }
